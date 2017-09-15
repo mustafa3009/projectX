0b99dff371ef431e4c7dc577742f90cd8594f6c8
Mustafa Khokhawala <mk@Mustafas-MacBook-Pro.local>
2017-09-15 09:02:35 -0400

    Amended the run script to include absolute path of docker-compose as jenkins was not able to find it

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo "Starting the docker container"
-cd docker && docker-compose up -d
+cd docker && /usr/local/bin/docker-compose up -d
 sleep 5s
 echo "Running tests"
 cd .. && python test/test.py --port 5000 --cert-path localcert/localhost.crt
